81dfdc6b57ccd47b3dc9c891a51f7d2a481d3067
BLAKE THOMSON <bdthomso@asu.edu>
2015-08-07 20:28:43 -0700

    Pulls from Reddit.com/r/listentothis. Users can add posts from reddit to their playlist.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,12 +2,33 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
+$scope.tracks = [];
+
+var refresh = function(){
 	$http.get('/player')
 		.success(function (response){
 			console.log('Got the data from server');
 			// $scope.tracks = response.tracks;
-			$scope.posts = response;		});
-		    
-	
+			$scope.posts = response;
+		});
+	};
+
+refresh(); 
+
+$scope.addToPlaylist = function(post){
+	var temp = {
+		name: post.title,
+		artist: 'placeholder',
+		plays: 0
+	}
+	console.log(temp);
+
+	$scope.tracks.push(temp);
+	refresh();
+}
+
+$scope.playIncrement = function(track){
+	track.plays += 1;
+}
 
 }]);
--- server.js
+++ server.js
@@ -1,64 +1,41 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('posts', ['posts'])
+//var db = mongojs('posts', ['posts']);
+var bodyParser = require('body-parser');
 // DB, [collection]
+var request = require('request');
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/player', function (req, res){
-	console.log('Received Get');
+	var posts = [];
+	request('http://www.reddit.com/r/listentothis.json', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var result = JSON.parse(body);
+	  	for(var i = 0; i < 12; i++){
+	  		if (result['data']['children'][i]['data']['domain'] !== 'self.listentothis'){
+	  			var tempPost = {
+		  			title: result['data']['children'][i]['data']['title'],
+		   			score: result['data']['children'][i]['data']['score'],
+		   			url: result['data']['children'][i]['data']['url']
+		   		}; 
+	   			posts.push(tempPost);
+	   		}; //End check for text posts
+	
+	  	};
+	  	res.json(posts);
+	  }
+	});
 
-	 	// post1 = {
-	  //   	title: 'Beat Fatigue -- Bearded Dragon [Electronic/Funk] (2014)',
-	  //   	votes: '55',
-	  //   	commentUrl: 'https://www.reddit.com/r/listentothis/comments/3frkkp/beat_fatigue_bearded_dragon_electronicfunk_2014/',
-	  //   	url: 'https://www.youtube.com/watch?v=8MPAr4EC-LA'
-	  //   };
 
-	  //   post2 = {
-	  //   	title: 'Brave Shores -- Never Come Down [Electropop] (2014)',
-	  //   	votes: '546',
-	  //   	commentUrl: 'https://www.reddit.com/r/listentothis/comments/3frkkp/beat_fatigue_bearded_dragon_electronicfunk_2014/',
-	  //   	url: 'https://www.youtube.com/watch?v=GCaYB2N1adc'
-	  //   };
 
-	  //   post3 = {
-	  //   	title: 'Wild Child -- Fools [Indie/Pop] (2015)',
-	  //   	votes: '42',
-	  //   	commentUrl: 'https://www.reddit.com/r/listentothis/comments/3frkkp/beat_fatigue_bearded_dragon_electronicfunk_2014/',
-	  //   	url: 'http://wildchildsounds.bandcamp.com/track/fools'
-	  //   };
-
-	  //   track1 = {
-	  //   	name: 'Bearded Dragon',
-	  //   	artist: 'Beat Fatigue', 
-	  //   	url: 'dummy',
-	  //   	plays: '4'
-	  //   };
-
-	  //   track2 = {
-	  //   	name: 'The Hills',
-	  //   	artist: 'The Weeknd', 
-	  //   	url: 'dummy',
-	  //   	plays: '1009'
-	  //   };
-
-	  //   track3 = {
-	  //   	name: 'Yellow',
-	  //   	artist: 'Coldplay', 
-	  //   	url: 'dummy',
-	  //   	plays: '45'
-	  //   };
-
-		// var posts = [post1, post2, post3];
-	 //    var tracks = [track1, track2, track3];
-
-	 db.posts.find(function (error, docs){
-	 	console.log(docs);
-	 	res.json(docs);
-	 });
+	 // db.posts.find(function (error, docs){
+	 // 	console.log(docs);
+	 // 	res.json(docs);
+	 // });
 
 	    // res.json({
 	    // 	posts: posts, 
@@ -68,4 +45,4 @@ app.get('/player', function (req, res){
 
 
 app.listen(5000);
-console.log("Server Running - 3000");
+console.log("RedditPlayer Server Running - 3000");
